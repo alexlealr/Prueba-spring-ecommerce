c54c765b4d404fcc3aa0cbb13a46f8c5c8fb58b8
alexlealr <ealealr@uqvirtual.edu.co>
2020-01-11 13:26:30 -0500

    Prueba spring

---
+++ appcore/src/test/java/co/ecommerce/core/AppcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ecommerce.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
